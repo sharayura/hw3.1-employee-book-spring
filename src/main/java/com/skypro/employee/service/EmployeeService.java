eb2816b11d5002bffd0a55abb6657ee17ef49110
sharayura <sharapyura@gmail.com>
2022-11-15 21:09:37 +0300

    Initial commit

---
+++ src/main/java/com/skypro/employee/service/EmployeeService.java
@@ -0,0 +1,36 @@
+package com.skypro.employee.service;
+
+import com.skypro.employee.model.Employee;
+import com.skypro.employee.record.EmployeeRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class EmployeeService {
+    private final Map<Integer, Employee> employees = new HashMap<>();
+
+    public Collection<Employee> getAllEmployees() {
+        return this.employees.values();
+    }
+
+    public Employee addEmployee(EmployeeRequest employeeRequest) {
+        if (employeeRequest.getFirstName() == null || employeeRequest.getLastName() == null) {
+            throw new IllegalArgumentException("Заполните имя");
+        }
+        Employee employee = new Employee(employeeRequest.getFirstName(), employeeRequest.getLastName(),
+                employeeRequest.getDepartment(), employeeRequest.getSalary());
+        this.employees.put(employee.getId(), employee);
+        return employee;
+    }
+
+    public int getSalarySum() {
+        return employees.values().stream()
+                .mapToInt(e -> e.getSalary())
+                .sum();
+
+    }
+
+}
